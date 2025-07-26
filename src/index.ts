392448cb4094d320be2a4b370aa5fa5071e83e2c
caperaven <caperaven@gmail.com>
2025-07-26 08:19:13 -0700

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Re‑export the core manager and providers so consumers can do
+// `import { SchemaManager, HeaderProvider } from 'dynamic-react-schema';`
+export { SchemaManager } from './SchemaManager';
+export { default as HeaderProvider } from './providers/HeaderProvider';
+export { default as ButtonProvider } from './providers/ButtonProvider';
+export type { SchemaNode, Provider } from './types';
